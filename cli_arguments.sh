f5d5b9157635c6d86b635b2d364a5b3ff7314e9d
Abbas Ismael <aismoctar@gmail.com>
2025-05-11 21:44:49 +0100

    Create cli_arguments.sh

---
+++ cli_arguments.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+i=1
+
+# Tant qu'il reste des arguments
+while [ $# -gt 0 ]
+do
+    echo "Argument $i: $1"
+    shift       # Enlève le premier argument ($1 devient le suivant)
+    i=$((i + 1))
+done
