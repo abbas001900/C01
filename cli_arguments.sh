51fa950f38c1afc6d0ef4df3df21ee005c43de8d
Abbas Ismael <aismoctar@gmail.com>
2025-05-11 21:48:16 +0100

    Update cli_arguments.sh

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+read char
+
+set --char
 
 i=1
 
 # Tant qu'il reste des arguments
-while [ $# -gt 0 ]
-do
+while [ $# -gt 0 ]; do
     echo "Argument $i: $1"
     shift       # Enlève le premier argument ($1 devient le suivant)
     i=$((i + 1))
