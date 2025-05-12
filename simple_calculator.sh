a07dc8b8c164e55c07e7380bdc5b58bf52b95268
Abbas Ismael <aismoctar@gmail.com>
2025-05-12 09:49:45 +0100

    Update simple_calculator.sh

--- simple_calculator.sh
+++ simple_calculator.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-read nbre1
-read nbre2
-read op
+read nbre1 nbre2 op
 
 case "$op" in
     "+")
-        echo "Résultat :$((nbre1 + nbre2))"
+        echo "Résultat : $((nbre1 + nbre2))"
         ;;
     "-")
-        echo "Résultat :$((nbre1 - nbre2))"
+        echo "Résultat : $((nbre1 - nbre2))"
         ;;
     "*")
-        echo "Résultat :$((nbre1 * nbre2))"
+        echo "Résultat : $((nbre1 * nbre2))"
         ;;
     "/")
         if [ "$nbre2" -eq 0 ]; then
-        
+            echo "Erreur : division par zéro interdite"
         else
-            echo "Résultat :$((nbre1 / nbre2))"
+            echo "Résultat : $((nbre1 / nbre2))"
         fi
+        ;;
+    *)
+        echo "Opérateur invalide. Utilisez +, -, * ou /"
         ;;
 esac
